639e341377c43300ac9985ac2f01d6bb3d81af6f
nicolasbonnetdu13 <bonnet.nicolas.13@gmail.com>
2014-09-11 21:01:56 +0200

    change 3 before_restart

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,10 +1,8 @@
-Chef::Log.info("Running deploy/before_restart.rb")
-
-# map the environment_variables node to ENV
-node[:deploy].each do |application, deploy|
-  deploy[:environment_variables].each do |key, value|
-    Chef::Log.info("Setting ENV[#{key}] to #{value}")
-    ENV[key] = value
-    Chef::Log.info("#{key}:#{ENV[key]}")
-  end if deploy[:environment_variables]
+remote_file "/srv/www/bonnet_nicolas/shared/config/database.yml" do
+  source "https://raw.github.com/ei3kf/appl_rails/master/config/database.yml"
+  owner "root"
+  group "root"
+  mode 0644
 end
+
+run "cd /srv/www/bonnet_nicolas/current && /usr/local/bin/bundle exec rake assets:precompile"
