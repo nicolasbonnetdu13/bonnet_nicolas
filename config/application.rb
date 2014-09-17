cd242a0c6045c8b32a9e5de88805c5cc955eb5d8
nicolasbonnetdu13 <bonnet.nicolas.13@gmail.com>
2014-09-17 13:46:26 +0200

    compress css / js

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,6 +16,9 @@ module MyBlog
   config.assets.initialize_on_precompile = false  
   
   
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor = :uglify
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -26,11 +30,6 @@ module MyBlog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
-  # config.assets.enable = true
-#   
-  # config.assets.version = "1.0"
-    
     
   end
 end
